ca6539af9691bec6ddef572f4a1cd4a5449de4e5
Isabbelle <isabelle.muresan@gmail.com>
2021-07-05 21:00:36 +0200

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
 import Restaurantfetch from './components/Restaurantfetch';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Restaurantfetch />
     </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <p>Home</p>
+            <p>About</p>
+            </div>
+    )
+}
+
+export default Navbar;
