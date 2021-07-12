564b3b6566c58b48d372587bc4f915bbf9d66b7f
Isabbelle <isabelle.muresan@gmail.com>
2021-07-13 00:05:20 +0200

    deleted component

--- src/components/Restaurantfetch.jsx
+++ src/components/Restaurantfetch.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Restaurantfetch() {
   const [details, setDetails] = useState([]);
@@ -15,19 +16,22 @@ function Restaurantfetch() {
     };
     getRestaurants();
   }, []);
+
   return (
     <div className="restaurant-card">
       <ul>
         {details.map((item) => (
           <div className="restaurantdetails">
-            <img class="image" src={item.photos[0].links[0]} alt="icon" />
+            <img className="image" src={item.photos[0].links[0]} alt="icon" />
             <div className="servicehours">
               <p>{item.opening_hours.open_now ? " open " : " closed "}</p>
               <p>{item.pickup ? " pickup " : null} </p>
               <p>{item.delivery ? " delivery " : null}</p>
             </div>
             <p>{item.formatted_address}</p>
-            <p className="name">{item.name}</p>
+            <Link to={`/detailspage/${item.id}`}>
+              <p className="name">{item.name}</p>
+            </Link>
           </div>
         ))}
       </ul>
