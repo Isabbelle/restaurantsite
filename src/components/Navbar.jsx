e2ed67ced4eb46c7878c4c3162cd92d6d36472b7
Isabbelle <isabelle.muresan@gmail.com>
2021-07-16 21:59:12 +0200

    added link to root

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 function Navbar() {
     return (
-        <div className="navbar">
-            <p>Home</p>
-            <p>About</p>
-            </div>
-    )
+      <div className="navbar">
+        <Link to ="/">
+          <p>Home</p>
+        </Link>
+        <p>About</p>
+      </div>
+    );
 }
 
 export default Navbar;
